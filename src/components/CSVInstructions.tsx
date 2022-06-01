beb72fe2803811bcec518a29d2a24232725abc5c
Kasun Samarasinghe <kwsamarasinghe@gmail.com>
2022-06-01 15:03:26 +0200

    Fix: Typo and example link corrected

--- src/components/CSVInstructions.tsx
+++ src/components/CSVInstructions.tsx
@@ -3,11 +3,13 @@ import React from "react";
 const CSVInstructions = () => {
   return (
     <div className="instructions-container instructions-list">
-      <h3>CVS Format</h3>
+      <h3>CSV Format</h3>
       <p>
         Try this example with{" "}
         <a
-          href={"https://www.nextprot.org/entry/NX_P13569/"}
+          href={
+            "https://www.nextprot.org/entry/NX_P13569/gh/calipho-sib/VEP-community-tool"
+          }
           target={"_blank"}
           rel={"noreferrer"}
         >
